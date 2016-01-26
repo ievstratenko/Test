daf335998f92f058a11f91c992c05c10ca20bca5
Denys Ievstratenko <denys.ievstratenko@gettyimages.com>
2016-01-26 15:48:21 +0200

    My feature 2

--- src/main/app/controller/Controller.java
+++ src/main/app/controller/Controller.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	@Value("${myprop}")
+	@Value("${myprop}" )
 	Object value;
 
 	@Resource
